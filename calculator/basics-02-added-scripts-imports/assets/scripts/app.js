d4360c71dad72c6e397495beb1868af5c69148e6
funakoshiapi <silvakoshis@gmail.com>
2020-05-26 10:59:05 -0400

    deleted folders

---
+++ calculator/basics-02-added-scripts-imports/assets/scripts/app.js
@@ -0,0 +1,44 @@
+let defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndRunOutput(operator, beforeValue, calcValue) {
+  const calcDescription = `${beforeValue} ${operator} ${calcValue}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndRunOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndRunOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndRunOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndRunOutput("/", initialResult, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
--- javascript assignments/original/basics-assignment-1-start/assignment.js
+++
@@ -1,8 +0,0 @@
-let myNum = 7;
-let result;
-result = 18 + myNum;
-result = result - 1;
-result = result * 4;
-result = result / 10;
-alert(myNum);
-alert(result);
