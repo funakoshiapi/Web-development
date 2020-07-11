844c1f8d21441cb2d1e4c4c6c59e5f31699f823b
funakoshiapi <silvakoshis@gmail.com>
2020-07-10 20:18:17 -0400

    game logic completed, created log to console

--- monster killer/control-03-monster-killer-starting-project/assets/scripts/vendor.js
+++ monster killer/control-03-monster-killer-starting-project/assets/scripts/vendor.js
@@ -1,17 +1,22 @@
 const monsterHealthBar = document.getElementById('monster-health');
 const playerHealthBar = document.getElementById('player-health');
 const bonusLifeEl = document.getElementById('bonus-life');
+const bonusLifeResetEl = document.getElementById('bonus-life');
 
 const attackBtn = document.getElementById('attack-btn');
 const strongAttackBtn = document.getElementById('strong-attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
 
-function adjustHealthBars(maxLife) {
-  monsterHealthBar.max = maxLife;
-  monsterHealthBar.value = maxLife;
-  playerHealthBar.max = maxLife;
-  playerHealthBar.value = maxLife;
+function adjustHealthBars(enteredLife) {
+
+  maxValue = 100;
+  monsterHealthBar.max = maxValue;
+  monsterHealthBar.value = enteredLife;
+  playerHealthBar.max = maxValue;
+  playerHealthBar.value = enteredLife;
+
+
 }
 
 function dealMonsterDamage(damage) {
@@ -33,12 +38,18 @@ function increasePlayerHealth(healValue) {
 function resetGame(value) {
   playerHealthBar.value = value;
   monsterHealthBar.value = value;
+  resetBonusLife();
+
+
 }
 
 function removeBonusLife() {
+
   bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+
 }
 
 function setPlayerHealth(health) {
   playerHealthBar.value = health;
 }
+
